2c972d17f6b47fdc1ea023744b8189d67d70a72d
Tinsae-Birhan1 <tinsaebirhan7@gmail.com>
2022-04-08 12:02:53 -0700

    12,8

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,50 +1,24 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
 /**
- * main - Entry point
- *
+ * main - prints all the numbers of base 16 in lowercase,
+ * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+  int n;
 
-	while (p < 58)
-	{
-		while (q < 58)
-		{
-			while (y < 58)
-			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
-				}
-				y++;
-				x = 48;
-			}
-			q++;
-			q = p;
-			x = q + 1;
-		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
-	}
-	putchar('\n');
-	return (0);
+  char ch;
+
+  for (n = 48; n < 58; n++)
+  {
+    putchar(n);
+  }
+  for (ch = 'a'; ch <= 'f'; ch++)
+  {
+    putchar(ch);
+  }
+  putchar('\n');
+  return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
+ * main - prints all possible combinations of two two-digit numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int z;
+  int i;
+  int j;
 
-for (z = 48; z <= 57; z++)
-{
-putchar(z);
-}
-for (z = 97; z <= 102;z++)
-{
-putchar(z)
-}
-putchar('\n');
-return (0);
+  for (i = 0; i < 100; i++)
+  {
+    for (j = 0; j < 100; j++)
+    {
+      if (i < j)
+      {
+        putchar((i / 10) + 48);
+        putchar((i % 10) + 48);
+        putchar(' ');
+        putchar((j / 10) + 48);
+        putchar((j % 10) + 48);
+        if (i != 98 || j != 99)
+        {
+          putchar(',');
+          putchar(' ');
+        }
+      }
+    }
+  }
+  putchar('\n');
+  return (0);
 }
